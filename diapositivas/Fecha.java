50cc957898feec44fbdc5d5d520d0763cffd8a14
Luis Usero Reyes <l.usero.dam@gmail.com>
2014-11-05 12:09:21 +0100

    Cambios para hacer commit: 	new file:   diapositivas/Fecha.class 	new file:   diapositivas/Fecha.java

---
+++ diapositivas/Fecha.java
@@ -0,0 +1,47 @@
+public class Fecha{
+	//atributos de la clase Fecha
+	private int dia;
+	private int mes;
+	private int anno;
+	
+	//getters y setters
+	//getters
+	public int getDia(){
+		return this.dia;
+
+	}
+	public int getMes(){
+		return this.mes;
+	}	
+	public int getAnno(){
+		return this.anno;
+	}		
+	//setters
+	public void setDia(int d){
+			this.dia=d;
+		
+	}
+		public void setMes(int m){
+			this.mes=m;
+		
+	}
+		public void setAnno(int a){
+			this.anno=a;
+		
+	}
+//	@0verride
+	public String toString(){ //utiliza el metodo ToString para machacar el f1 que nos daba la posicion de memoria
+		return this.dia+"/"+this.mes+"/"+this.anno;
+	}
+public static void main (String[] args){
+	
+		Fecha f1 = new Fecha();
+		f1.setDia(5);
+		f1.setMes(11);
+		f1.setAnno(2014);
+		System.out.println("Fecha: " +f1.getDia()+"-" +f1.getMes()+"-" +f1.getAnno());
+		System.out.println("Fecha: " +f1);
+	
+	}
+}
+
